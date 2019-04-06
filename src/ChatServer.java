260059c93f9904a91a3d6e618f35f6162f0c751e
lizhenwei <lizhenwei.will@foxmail.com>
2019-04-06 16:22:49 +0800

    opt : 防止客户端异常关闭时对服务端产生影响

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -11,16 +11,39 @@ import java.net.Socket;
 public class ChatServer {
 
     public static void main(String[] args) {
+        new ChatServer().launchSerer();
+    }
+
+    /**
+     * 启动服务器程序
+     */
+    private void launchSerer() {
+
+        //  服务器端的 ServerSocket
+        ServerSocket serverSocket = null;
+
+        //  客户端的 Socket
+        Socket socket = null;
+
+        //  网络数据输入流
+        DataInputStream dis = null;
+
+        boolean serverStarted = false;
         try {
-            boolean serverStarted;
-            ServerSocket ss = new ServerSocket(8888);
+            serverSocket = new ServerSocket(8888);
+            System.out.println("ServerSocket Start");
             serverStarted = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
             while (serverStarted) {
-                boolean clientStarted;
-                Socket socket = ss.accept();
+                boolean clientStarted = false;
+                socket = serverSocket.accept();
                 System.out.println("a client connected");
                 clientStarted = true;
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                dis = new DataInputStream(socket.getInputStream());
                 while (clientStarted) {
                     String readUTF = dis.readUTF();
                     System.out.println(readUTF);
@@ -31,9 +54,21 @@ public class ChatServer {
                 }
                 dis.close();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Client is closed");
+        } finally {
+            try {
+                if (dis != null) {
+                    dis.close();
+                }
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
+
     }
 
 }
