403410b1721bdcbbb57872268a5aff95813fc2b9
lizhenwei <lizhenwei.will@foxmail.com>
2019-04-06 21:48:14 +0800

    opt : 服务端代码完成，测试通过

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,9 +1,13 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : zhenweiLi
@@ -12,14 +16,19 @@ import java.net.Socket;
  */
 public class ChatServer {
 
+    /**
+     * 所有客户端集合
+     */
+    private List<Client> mClientList;
+
     public static void main(String[] args) {
-        new ChatServer().launchSerer();
+        new ChatServer().launchServer();
     }
 
     /**
      * 启动服务器程序
      */
-    private void launchSerer() {
+    private void launchServer() {
 
         //  服务器端的 ServerSocket
         ServerSocket serverSocket = null;
@@ -30,6 +39,7 @@ public class ChatServer {
             serverSocket = new ServerSocket(8888);
             System.out.println("ServerSocket Start");
             serverStarted = true;
+            mClientList = new ArrayList<>();
         } catch (BindException e) {
             System.out.println("端口已经被使用");
             System.out.println("请退出相关程序并重新启动服务端");
@@ -41,7 +51,8 @@ public class ChatServer {
         try {
             while (serverStarted) {
                 Client client = new Client(serverSocket.accept());
-                new Thread(client).run();
+                mClientList.add(client);
+                new Thread(client).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -72,14 +83,40 @@ public class ChatServer {
          */
         private DataInputStream mDis;
 
-        boolean mClientStarted;
+        /**
+         * 网络数据输出流
+         */
+        private DataOutputStream mDos;
+
+        /**
+         * 客户端连接状态
+         */
+        private boolean mClientStarted;
 
         Client(Socket socket) {
             this.mSocket = socket;
             try {
                 this.mDis = new DataInputStream(socket.getInputStream());
+                this.mDos = new DataOutputStream(socket.getOutputStream());
                 System.out.println("a client connected");
                 mClientStarted = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 客户端在线时，才需要推送消息
+         *
+         * @param message 消息内容
+         */
+        void send(String message) {
+            try {
+                mDos.writeUTF(message);
+                mDos.flush();
+            } catch (SocketException e) {
+                mClientList.remove(this);
+                System.out.println("对方已下线");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -91,18 +128,24 @@ public class ChatServer {
                 while (mClientStarted) {
                     String readUTF = mDis.readUTF();
                     System.out.println(readUTF);
-                    if ("exit".equals(readUTF)) {
-                        mClientStarted = false;
+                    for (Client client : mClientList) {
+                        if (client == this) {
+                            continue;
+                        }
+                        client.send(readUTF);
                     }
                 }
             } catch (EOFException e) {
-                System.out.println("Client is closed");
+                System.out.println("客户端已关闭");
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 try {
                     if (mDis != null) {
                         mDis.close();
+                    }
+                    if (mDos != null) {
+                        mDos.close();
                     }
                     if (mSocket != null) {
                         mSocket.close();
